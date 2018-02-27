f357bccea2a4274c5e123608e982f5743054a1b0
Wali Waezi <maiwalw@gmail.com>
2018-02-27 09:38:32 +0100

    ajout du fichier sql

--- poophp.sql
+++ poophp.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 23 Février 2018 à 16:08
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 27 fév. 2018 à 08:36
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `poophp`
@@ -26,21 +28,28 @@ SET time_zone = "+00:00";
 -- Structure de la table `personnagestp`
 --
 
+DROP TABLE IF EXISTS `personnagestp`;
 CREATE TABLE IF NOT EXISTS `personnagestp` (
   `id` smallint(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) CHARACTER SET utf8 NOT NULL,
   `degats` tinyint(3) NOT NULL DEFAULT '0',
+  `experience` tinyint(3) NOT NULL DEFAULT '0',
+  `niveau` tinyint(3) NOT NULL DEFAULT '1',
+  `puissance` tinyint(3) NOT NULL DEFAULT '5',
+  `nbCoupsPortes` int(11) NOT NULL DEFAULT '0',
+  `dateDernierCoupPorte` date NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `personnagestp`
+-- Déchargement des données de la table `personnagestp`
 --
 
-INSERT INTO `personnagestp` (`id`, `nom`, `degats`) VALUES
-(12, 'Sam', 90),
-(13, 'Jack', 0);
+INSERT INTO `personnagestp` (`id`, `nom`, `degats`, `experience`, `niveau`, `puissance`, `nbCoupsPortes`, `dateDernierCoupPorte`) VALUES
+(75, 'henry', 5, 80, 8, 12, 3, '2018-02-26'),
+(76, 'wai', 99, 40, 1, 5, 1, '2018-02-26');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
